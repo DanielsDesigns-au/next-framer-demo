57832ef036a1e5a8705bc2e8c01d042aceffa475
GoldenSeven <62866680+GoldenSeven@users.noreply.github.com>
2021-11-04 18:18:31 +0800

    Demo Test Of Tick

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  urlImports: [
+    "https://fonts.googleapis.com/",
+  ],
+  experimental: {
+    urlImports: [
+      "https://framer.com/m/",
+      "https://framerusercontent.com/",
+      "https://ga.jspm.io/",
+      "https://jspm.dev/",
+    ],
+  },
 }
